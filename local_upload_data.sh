3e36ecfb45bc5740b2884eed17612c9a3847a416
Mick Powell <celeryness@users.noreply.github.com>
2020-08-15 12:12:28 -0600

    removing files that I wanted ignored

--- local_upload_data.sh
+++
@@ -1,3 +0,0 @@
-export GOOGLE_APPLICATION_CREDENTIALS=~/.google/VVTrial-2afe54fb2ed0.json
-php ../bq-upload/bigquery/api/bigquery.php --project="vv-trial" import covid_guatemala.casos_integrados for_upload/casos_integrados.json  --overwrite=true
-##php bigquery/api/bigquery.php --project="vv-trial" import viaventure_sales.sac_resource_type_current_year %~dp0SalesAndConversions\sac_rt_current_year.json  --overwrite=true
